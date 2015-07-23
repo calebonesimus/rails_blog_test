a61994f281c48aefc14cc7d383c01f71b716f67b
Caleb Francis <calebonesimus@gmail.com>
2015-07-22 21:18:27 -0400

    Fixed issue where view_blog wouldn't work without db

--- app/controllers/view_blog_controller.rb
+++ app/controllers/view_blog_controller.rb
@@ -1,7 +1,11 @@
 class ViewBlogController < ApplicationController
   def view
-    @blog_title = BlogPost.last.blog_title
-    @date_pub = BlogPost.last.created_at
-    @blog_content = BlogPost.last.blog_content
+    if BlogPost.exists?
+      @blog_title = BlogPost.last.blog_title
+      @date_pub = BlogPost.last.created_at
+      @blog_content = BlogPost.last.blog_content
+    else
+      render template: 'view_blog/view'
+    end
   end
 end
